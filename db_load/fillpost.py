d033fdf6d3283982ed338f7d76ef18518d2e3a33
James Gallmeister <jgallmeister@Jamess-MacBook-Pro-2.local>
2018-10-30 17:30:32 -0600

    added comments and config options to fill script

--- db_load/fillpost.py
+++ db_load/fillpost.py
@@ -1,29 +1,46 @@
 import psycopg2 as post
+import sys
 import csv
 import to_csv
 
 to_add = []
 config = {
-	'host':'localhost',
-	'dbname':'password',
-	'user':'jamgall'
+	'host':'localhost'
 }
+con_keys = ['dbname', 'user']
 
+# writes the configurations for connecting to the postgres db
+def write_config():
+	global config
+	for key in con_keys:
+		if key not in config:
+			resp = raw_input('Please enter the exact %s you are using: ' % key)
+			config[key] = resp
+
+# allows the user to convert the .txt file to csv and add it to the db line by line
 def main():
 	to_csv.main()
-	conn = post.connect(**config)
+	write_config()
+	# if there are connection errors, handle them and print them out to the console
+	try:
+		conn = post.connect(**config)
+	except post.OperationalError as e:
+		print('Unable to connect!\n{0}').format(e)
+		sys.exit(1)
 	cnx = conn.cursor()
 	print('Connected!')
 	filename = raw_input('Please enter name of csv file to read in: ')
 	with open(filename, 'r') as file:
 		csvread = csv.reader(file)
+		print('loading database...')
 		for row in csvread:
 			#print row
 			add = row[0]
 			if add.find("'") == -1:
 				cnx.execute("INSERT INTO pass (word) VALUES (\'%s\');" % (add))
-	print 'HEY LOOK AT THAT IT FUCKING WORKED'
+	print('...database loaded')
 	conn.commit()
+	cursor.close()
 	return 0
 
 
